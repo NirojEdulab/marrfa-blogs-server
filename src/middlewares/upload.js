b1a81173a6777b7d7a828c2ef6ed0662c5f71119
nirojEdulab <niroj@edulab.in>
2024-12-07 16:50:46 +0530

    modified multer

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,29 +1,16 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
 const fs = require('fs');
+const path = require('path');
 
-// Ensure 'uploads' directory exists
-const uploadDir = path.join(__dirname, '../uploads');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir, { recursive: true }); // Creates the folder if it doesn't exist
-}
-
-// Configure multer to store images temporarily
 const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir); // Use the ensured 'uploads' directory
+  destination: function (req, file, cb) {
+    const dir = path.join('public', 'temp');
+    fs.mkdirSync(dir, { recursive: true });
+    cb(null, "./public/temp");
   },
-  filename: (req, file, cb) => {
-    cb(null, `${Date.now()}-${file.originalname}`);
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
   },
 });
 
-const fileFilter = (req, file, cb) => {
-  if (file.mimetype.startsWith('image/')) {
-    cb(null, true);
-  } else {
-    cb(new Error('Invalid file type. Only images are allowed'), false);
-  }
-};
-
-module.exports = multer({ storage, fileFilter });
+module.exports = multer({ storage });
